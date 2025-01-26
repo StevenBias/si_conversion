a797bf7eaa9b7289597515f4e0384ceeb9d094cf
Steven Bias <bias_steven@hotmail.fr>
2025-01-26 11:47:41 +0100

    Send 'Hello' and 'World' as argument and print 'Hello, World!'

--- pounds_to_kilos.zig
+++ pounds_to_kilos.zig
@@ -3,5 +3,10 @@ const std = @import("std");
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     const args = try std.process.argsAlloc(std.heap.page_allocator);
-    try stdout.print("{s}, {s}!\n", .{args[1], "World"});
+
+    // Do not forget the first argument is the program name
+    // Ask for exactly two other arguments
+    if (args.len != 3) return error.ExpectedArgument;
+
+    try stdout.print("{s}, {s}!\n", .{args[1], args[2]});
 }
