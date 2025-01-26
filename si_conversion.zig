0f4823483415da08cf9a3ff2ce008ae285b264c3
Steven Bias <bias_steven@hotmail.fr>
2025-01-26 20:09:58 +0100

    Add fahrenheit to celsius conversion

--- si_conversion.zig
+++ si_conversion.zig
@@ -7,6 +7,7 @@ const stdin = std.io.getStdIn().reader();
 const Units = enum(u8) {
     pounds = 1,
     kilograms = 2,
+    fahrenheit = 3,
     max
 };
 
@@ -32,11 +33,22 @@ fn new_unit(unit: Units) Unit_t {
                 .conv_symbol = "lb",
             };
         },
+        Units.fahrenheit => {
+            return Unit_t {
+                .unit = unit,
+                .symbol = "°F",
+                .conv_symbol = "°C",
+            };
+        },
         else => unreachable,
     }
 }
 
 const lb_kg_conversion = 0.45359237;
+
+fn fahrenheit_to_celsius(fahr: f32) f32 {
+    return (fahr - 32) * (5.0 / 9.0);
+}
 
 fn kilos_to_pounds(kilos: f32) f32 {
     return (kilos / lb_kg_conversion);
@@ -68,6 +80,9 @@ fn convert(sel: u8) !void {
             Units.kilograms => {
                 out_val = kilos_to_pounds(in_val);
             },
+            Units.fahrenheit => {
+                out_val = fahrenheit_to_celsius(in_val);
+            },
             else => unreachable,
         }
 
@@ -85,6 +100,7 @@ pub fn main() !void {
         \\
         \\[1] - pounds to kilograms
         \\[2] - kilograms to pounds
+        \\[3] - fahrenheit to celsius
         \\
     });
 
