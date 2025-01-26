18acf7eb166d3467be7cbd15d7a3ccba29ac435d
Steven Bias <bias_steven@hotmail.fr>
2025-01-26 12:06:02 +0100

    Defer args variable to free memory at the end of prog

--- pounds_to_kilos.zig
+++ pounds_to_kilos.zig
@@ -1,8 +1,12 @@
 const std = @import("std");
 
 pub fn main() !void {
+    // Add a writter to stdout
     const stdout = std.io.getStdOut().writer();
+
     const args = try std.process.argsAlloc(std.heap.page_allocator);
+    // We can be sure our args' memory is freed when main is returned from
+    defer std.process.argsFree(std.heap.page_allocator, args);
 
     // Do not forget the first argument is the program name
     // Ask for exactly two other arguments
